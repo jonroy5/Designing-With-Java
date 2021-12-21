f085d7d0ea70f7a6d55299d7f82d1599b8d53369
Jon Roy <jroy5@comcast.net>
2021-12-21 18:36:32 -0500

    sprint3.7

--- src/com/amazon/ata/types/PolyBag.java
+++ src/com/amazon/ata/types/PolyBag.java
@@ -33,7 +33,7 @@ public class PolyBag extends Packaging {
     @Override
     public BigDecimal getMass() {
 
-        return BigDecimal.valueOf(Math.ceil(Math.sqrt(volume.doubleValue() * 0.6));
+        return BigDecimal.valueOf(Math.ceil(Math.sqrt(volume.doubleValue()) * 0.6));
     }
 
     @Override
